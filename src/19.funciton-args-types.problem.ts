9469e80d91b48b5b2e782c31f158428602d7fdb0
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-10 13:41:32 +0200

    feat: create type based on function parameters

---
+++ src/19.funciton-args-types.problem.ts
@@ -0,0 +1,34 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+// TODO: fix typings for MakeQueryParameters
+const makeQuery = (
+  url: string,
+  opts?: {
+    method?: string;
+    headers?: {
+      [key: string]: string;
+    };
+    body?: string;
+  },
+) => {};
+
+type MakeQueryParameters = Parameters<typeof makeQuery>;
+type Opts = MakeQueryParameters[1];
+
+type tests = [
+  Expect<
+    Equal<
+      MakeQueryParameters,
+      [
+        url: string,
+        opts?: {
+          method?: string;
+          headers?: {
+            [key: string]: string;
+          };
+          body?: string;
+        },
+      ]
+    >
+  >,
+];
