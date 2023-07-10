32f6f0c5efec68e999c2eb14abbf547ebeac7f01
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-10 11:19:50 +0200

    feat: record type

---
+++ src/11-record.problem.ts
@@ -0,0 +1,46 @@
+import { expect, it } from "vitest";
+
+// TODO: fix typings
+interface Cachabl {
+  [id: string]: string;
+}
+
+const createCache = () => {
+  // const cache: Cachabl = {};
+  const cache: Record<string, string> = {};
+  // const cache: Record<string, {
+  //   value: string,
+  //   isAdmin: boolean
+  // }> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "John");
+
+  expect(cache.cache["123"]).toEqual("John");
+});
+
+it("Should remove values from the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "John");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
