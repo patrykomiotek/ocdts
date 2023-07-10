4edaebea0c8c637dc38c9d172c67697863873d53
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-10 09:44:22 +0200

    feat: optional types in arguments

---
+++ src/03-optional-properties.problem.ts
@@ -0,0 +1,29 @@
+import { expect, it } from "vitest";
+
+// TODO: fix typings with optional value
+
+export const getName = (params: { first: string; last?: string }) => {
+// export const getName = (params: { first: string; last: string | undefined }) => {
+  if (params.last) {
+    return `${params.first} ${params.last}`;
+  }
+  return params.first;
+};
+
+it("Should work with just the first name", () => {
+  const name = getName({
+    first: "John",
+    // last: undefined,
+  });
+
+  expect(name).toEqual("John");
+});
+
+it("Should work with the first and last name", () => {
+  const name = getName({
+    first: "John",
+    last: "Doe",
+  });
+
+  expect(name).toEqual("John Doe");
+});
