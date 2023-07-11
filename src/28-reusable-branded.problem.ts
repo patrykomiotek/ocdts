ef7ae85737d1b4225c10a54701449099f6616559
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-11 09:50:59 +0200

    feat: valid values

---
+++ src/28-reusable-branded.problem.ts
@@ -0,0 +1,37 @@
+import { it } from "vitest";
+import { Brand } from "./helpers/Brand";
+
+// TODO: fix types for Valid and validatePassword
+
+type Valid<T> = Brand<T, 'Valid'>;
+
+interface PasswordValues {
+  password: string;
+  confirmPassword: string;
+}
+
+const validatePassword = (values: PasswordValues) => {
+  if (values.password !== values.confirmPassword) {
+    throw new Error("Passwords do not match");
+  }
+
+  return values;
+};
+
+const createUserOnApi = (values: Valid<PasswordValues>) => {
+  // Imagine this function creates the user on the API
+};
+
+it("Should fail if you do not validate the values before calling createUserOnApi", () => {
+  const onSubmitHandler = (values: PasswordValues) => {
+    // @ts-expect-error
+    createUserOnApi(values);
+  };
+});
+
+it("Should succeed if you DO validate the values before calling createUserOnApi", () => {
+  const onSubmitHandler = (values: PasswordValues) => {
+    const validatedValues = validatePassword(values);
+    createUserOnApi(validatedValues);
+  };
+});
