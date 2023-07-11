58f79e054c179bd6dab05a54f17de793299887e2
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-11 09:43:53 +0200

    feat: branded types in fetching

--- src/25-branded.explainer.ts
+++ src/25-branded.explainer.ts
@@ -1,24 +1,27 @@
-import { Brand } from "../helpers/Brand";
+import { Brand } from "./helpers/Brand";
+
+// type Password = string;
+// type Email = string;
 
 type Password = Brand<string, "Password">;
 type Email = Brand<string, "Email">;
 
-// type UserObject = Brand<
-//   {
-//     id: string;
-//     name: string;
-//   },
-//   "User"
-// >;
+type UserObject = Brand<
+  {
+    id: string;
+    name: string;
+  },
+  "User"
+>;
 
-// const user: UserObject = {
-//   id: "awdawd",
-//   name: "awdawdawd",
-// } as UserObject;
-
-const verifyPassword = (password: Password) => {};
+const user: UserObject = {
+  id: "5",
+  name: "ahjdassdh",
+} as UserObject;
 
 const password = "1231423" as Password;
+
+const verifyPassword = (password: Password) => {};
 
 const email = "sth@example.com" as Email;
 
